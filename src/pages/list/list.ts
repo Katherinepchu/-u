ceceaf0734ab2a785caa3ad6cdd5d3fd7e3e2ca5
root <root@Jose.Jose>
2018-11-22 11:22:33 -0500

    exitos1

--- src/pages/list/list.ts
+++ src/pages/list/list.ts
@@ -5,6 +5,7 @@ import { NavController, NavParams } from 'ionic-angular';
 import { LoggedinPage } from "../loggedin/loggedin";
 
 import { Proveedor1Provider } from '../../providers/proveedor1/proveedor1';
+import { MovieApiProvider } from "../../providers/movie-api/movie-api";
 import { IMovie } from "../../interface/IMovie";
 @Component({
   selector: 'page-list',
@@ -12,8 +13,11 @@ import { IMovie } from "../../interface/IMovie";
 })
 export class ListPage {
   usuarios = new Array<IMovie>();
+  movies = new Array<IMovie>();
+
   
-  constructor(public navCtrl: NavController, public navParams: NavParams, public proveedor:Proveedor1Provider) {
+  constructor(public navCtrl: NavController, public navParams: NavParams, public proveedor:Proveedor1Provider,
+    private movieApiProvider: MovieApiProvider) {
     
   }
 
@@ -27,6 +31,10 @@ ionViewDidLoad(){
          (error)=> {console.log(error);}
 
          )
+
+      this.movieApiProvider.getMovies().subscribe(data => {
+      this.movies = data;
+    });
      }
 
 
@@ -34,6 +42,7 @@ ionViewDidLoad(){
     this.navCtrl.push(LoggedinPage, movie);
   }
 
+  
 
 
     
