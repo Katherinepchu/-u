31c4bebe50be57938a264ae4f680f5f365710a7f
katherine <katy.pillaca1@gmail.com>
2018-11-23 08:29:56 -0500

    'b'

---
+++ src/pages/list/list.ts
@@ -0,0 +1,45 @@
+import { Component } from '@angular/core';
+
+import { NavController, NavParams } from 'ionic-angular';
+
+import { LoggedinPage } from "../loggedin/loggedin";
+
+import { Proveedor1Provider } from '../../providers/proveedor1/proveedor1';
+
+import { IMovie } from "../../interface/IMovie";
+@Component({
+  selector: 'page-list',
+  templateUrl: 'list.html'
+})
+export class ListPage {
+  usuarios
+  movies = new Array<IMovie>();
+  nombre=""
+
+  
+  constructor(public navCtrl: NavController, public navParams: NavParams, public proveedor:Proveedor1Provider) {
+    
+  }
+
+ionViewDidLoad(){
+       this.proveedor.obtenerDatos()
+       .subscribe(
+         (data)=> {this.usuarios = data['records'];
+         console.log('Tigrezhito-traeDatos',data)
+
+         },
+         (error)=> {console.log(error);}
+
+         )
+
+    
+     }  
+
+
+   goToDetail(movie: IMovie) {
+    this.navCtrl.push(LoggedinPage, movie);
+  }
+
+
+    
+}
